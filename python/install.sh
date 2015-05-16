ed97931c318fa930f7bfa10990c8a1b74fc11b44
simpsonw <wsimpson@email.arizona.edu>
2015-05-15 20:23:27 -0700

    Add fonts for powerline.

--- python/install.sh
+++ python/install.sh
@@ -1,1 +1,8 @@
 pip install powerline-status
+
+# Install fonts for Powerline
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+mv PowerlineSymbols.otf /usr/share/fonts/X11/misc/
+fc-cache -vf /usr/share/fonts/X11/misc/
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
