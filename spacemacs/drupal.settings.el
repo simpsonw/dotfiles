6bd505389fac5e2709032f86a097d405fd3d2c4a
Will Simpson <wsimpson@email.arizona.edu>
2015-12-04 14:20:22 -0700

    Add custom settings.el files for Drupal and PSR standard.

---
+++ spacemacs/drupal.settings.el
@@ -0,0 +1,6 @@
+(set 'tab-width 2)
+(set 'c-basic-offset 2)
+(set 'indent-tabs-mode nil)
+(c-set-offset 'case-label '+)
+(c-set-offset 'arglist-intro '+) ; for FAPI arrays and DBTNG
+(c-set-offset 'arglist-cont-nonempty 'c-lineup-math) ; for DBTNG fields and values
---
+++ spacemacs/psr.settings.el
@@ -0,0 +1,3 @@
+(setq c-basic-indent 4)
+(setq tab-width 4)
+(setq indent-tabs-mode nil)
