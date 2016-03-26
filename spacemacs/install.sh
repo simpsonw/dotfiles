86fdf1af5395c232f676c4b11dba077d0b92e0c4
Will Simpson <wsimpson@email.arizona.edu>
2016-03-25 17:53:04 -0700

    Build GNU Emacs 24.5.1 from source before getting Spacemacs.

--- spacemacs/install.sh
+++ spacemacs/install.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+sudo apt-get install build-essential
+sudo apt-get build-dep emacs24
+wget http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.gz
+tar xf emacs-24.5.tar.gz
+cd emacs-24.5/
+./configure
+make
+sudo make install
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
