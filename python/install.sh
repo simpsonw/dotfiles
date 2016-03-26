16163a3ab84fd6247df46314e902308f6346ccf1
Will Simpson <wsimpson@email.arizona.edu>
2016-03-25 18:04:56 -0700

    Remove python directory since I'm no longer using powerline.

--- python/install.sh
+++
@@ -1,8 +0,0 @@
-sudo pip install powerline-status
-
-# Install fonts for Powerline
-wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
-wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
-sudo mv PowerlineSymbols.otf /usr/share/fonts/X11/misc/
-fc-cache -vf /usr/share/fonts/X11/misc/
-sudo mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
