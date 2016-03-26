59828ff6a6319a7954b1e20a8475c5077b5102ab
Will Simpson <wsimpson@email.arizona.edu>
2016-03-25 18:05:16 -0700

    Increase the number of jobs in make and clean up after install

--- spacemacs/install.sh
+++ spacemacs/install.sh
@@ -5,6 +5,7 @@ wget http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.gz
 tar xf emacs-24.5.tar.gz
 cd emacs-24.5/
 ./configure
-make
+make -j=2
 sudo make install
+rm -rf emacs-24.5*
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
