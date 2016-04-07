cefe4cc120f59fe8600d8b0a9ec14649aa8cd1c8
Will Simpson <wsimpson@email.arizona.edu>
2016-04-07 15:03:44 -0700

    Replace gnome-terminal-profile with gnome-terminal-colors installer.

---
+++ gnome-terminal-colors/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git clone https://github.com/metalelf0/gnome-terminal-colors.git
+cd gnome-terminal-colors
+./install.sh -s dracula
--- gnome-terminal-profile/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install fonts-inconsolata
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ln -s ${DIR}/will/ ~/.gconf/apps/gnome-terminal/profiles/
