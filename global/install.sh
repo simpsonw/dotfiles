fffa99222078d9d0f90737758b6a4f2310eceab2
Will Simpson <wsimpson@email.arizona.edu>
2016-06-08 15:56:32 -0700

    Fix symbolic link, add depency, and clean up files for GNU Global.

--- global/install.sh
+++ global/install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 sudo apt-get install exuberant-ctags
-ln -s /usr/bin/ctags-exuberant /usr/local/bin/ctags
+sudo ln -s /usr/bin/ctags-exuberant /usr/local/bin/ctags
+sudo apt-get install libncurses-dev
 wget ftp://ftp.gnu.org/pub/gnu/global/global-6.5.4.tar.gz
 tar xvf global-6.5.4.tar.gz
 cd global-6.5.4/
 ./configure --with-exuberant-ctags=/usr/local/bin/ctags
 make
 sudo make install
+cd ../
+rm -rf global-6.5.4*
