6ae5eb1bcde3823ba2b74ae91e518c4f4792d179
Will Simpson <wsimpson@email.arizona.edu>
2017-04-20 17:32:30 -0700

    Restart NeoVim configuration

--- nvim/install.sh
+++ nvim/install.sh
@@ -2,3 +2,10 @@
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
+pip install --upgrade pip
+pip install --user setuptools
+pip install --user neovim
