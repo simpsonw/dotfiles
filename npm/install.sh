af39c41206d8c3adae3d0443c93acdf49185c202
Will Simpson <wsimpson@email.arizona.edu>
2017-08-31 16:31:53 -0700

    Install vmd package for use with Spacemacs markdown preview.

--- npm/install.sh
+++ npm/install.sh
@@ -4,3 +4,7 @@ sudo apt-get install npm
 sudo npm install -g tern
 sudo npm install -g js-beautify
 sudo npm install -g jshint
+
+# Packages needed for Spacemacs markup layer
+sudo npm install -g vmd
+sudo ln -s /usr/bin/vmd /usr/bin/markdown
