3587d0c357d9e46a497d6d6d894ec357b7a75035
Will Simpson <wsimpson@email.arizona.edu>
2017-11-20 10:36:42 -0700

    Upgrade version of Emacs to 25.3.

--- spacemacs/emacs-install.sh
+++ spacemacs/emacs-install.sh
@@ -7,13 +7,13 @@ read response
 if [ "$response" == "y" ]; then
 	sudo apt-get install build-essential
 	sudo apt-get build-dep emacs24
-	wget http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.gz
-	tar xf emacs-24.5.tar.gz
-	cd emacs-24.5/
+	wget http://ftp.gnu.org/gnu/emacs/emacs-25.3.tar.gz
+	tar xf emacs-25.3.tar.gz
+	cd emacs-25.3/
 	./configure
 	make -j=2
 	sudo make install
 	cd ../
-	sudo rm -rf emacs-24.5*
+	sudo rm -rf emacs-25.3*
 	git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 fi
