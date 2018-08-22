9c515358d1e266c1f674c9bbf85abc1bdb707cb4
Will Simpson <wsimpson@email.arizona.edu>
2018-08-22 16:22:45 -0700

    Add support for Go.

---
+++ golang/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo apt install golang-go
+go get -u -v github.com/nsf/gocode
+go get -u -v github.com/rogpeppe/godef
+go get -u -v golang.org/x/tools/cmd/guru
+go get -u -v golang.org/x/tools/cmd/gorename
+go get -u -v golang.org/x/tools/cmd/goimports
+echo "source ~/.golang.bash" >> ~/.bashrc
