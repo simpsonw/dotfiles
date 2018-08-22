1a6502e1a5acc3bc14d46993fae45cb395a76a59
Will Simpson <wsimpson@email.arizona.edu>
2018-08-22 16:41:50 -0700

    Rename golang bash file.

--- golang/install.sh
+++ golang/install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+echo "source ~/.golang.bash" >> ~/.bashrc
+source ~/.bashrc
 sudo apt install golang-go
 go get -u -v github.com/nsf/gocode
 go get -u -v github.com/rogpeppe/godef
 go get -u -v golang.org/x/tools/cmd/guru
 go get -u -v golang.org/x/tools/cmd/gorename
 go get -u -v golang.org/x/tools/cmd/goimports
-echo "source ~/.golang.bash" >> ~/.bashrc
