591983ce3144ef366b80b7a5df94555f49099aa7
Will Simpson <wsimpson@email.arizona.edu>
2018-09-27 13:02:10 -0700

    Add betterlockscreen for i3.

--- i3/install.sh
+++ i3/install.sh
@@ -5,3 +5,6 @@ sudo apt update
 sudo apt-get install rofi
 sudo apt-get install feh
 feh --bg-center jupiter.png
+git clone git@github.com:UtkarshVerma/installer-scripts.git i3lock-installer
+chmod +x i3lock-installer/betterlockscreen.sh
+i3lock-installer/betterlockscreen.sh
