a9a5826a102ed707ee1bbbbff6bf0b2bc2ff0fcd
Will Simpson <wsimpson@email.arizona.edu>
2018-10-19 15:25:54 -0700

    Fix path to wallpaper for i3/feh.

--- i3/install.sh
+++ i3/install.sh
@@ -4,7 +4,7 @@ sudo add-apt-repository ppa:jasonpleau/rofi
 sudo apt update
 sudo apt-get install rofi
 sudo apt-get install feh
-feh --bg-center wallpaper.png
+feh --bg-center $PWD/wallpaper.png
 git clone git@github.com:UtkarshVerma/installer-scripts.git i3lock-installer
 chmod +x i3lock-installer/betterlockscreen.sh
 i3lock-installer/betterlockscreen.sh
