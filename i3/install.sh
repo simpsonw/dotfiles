7f5c83010f77c266d94a00bd2275a8f9f66ecd59
Will Simpson <wsimpson@email.arizona.edu>
2019-01-08 17:11:02 -0700

    Add cinnamon-i3.

--- i3/install.sh
+++ i3/install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+if ! hash npm 2>/dev/null; then
+    bash ../npm/install.sh
+fi
+
 sudo apt-get install i3
 sudo add-apt-repository ppa:jasonpleau/rofi
 sudo apt update
 sudo apt-get install rofi
 sudo apt-get install feh
-feh --bg-center wallpaper.png
-git clone git@github.com:UtkarshVerma/installer-scripts.git i3lock-installer
-chmod +x i3lock-installer/betterlockscreen.sh
-i3lock-installer/betterlockscreen.sh
+feh --bg-center $PWD/wallpaper.png
+git clone git@github.com:krusipo/cinnamon-i3.git
+cd cinnamon-i3
+npm install
+node_modules/grunt/bin/grunt
+grunt debian
+sudo dpkg -i dist/debian/cinnamon-i3_0.5.0-1_amd64.deb
+sudo apt-get -f install
+rm -rf cinnamon-i3
