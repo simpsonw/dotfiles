5daf540c8ac65ecdea678480be163ad9a3d0974e
Will Simpson <wsimpson@email.arizona.edu>
2019-05-06 15:14:39 -0700

    Revert "Remove Cinnamon specific stuff for i3."
    This reverts commit b6ced0d1947254eadf2c9a4748f9827b4fd02d2f.

--- i3/install.sh
+++ i3/install.sh
@@ -10,4 +10,12 @@ sudo apt-get install rofi
 sudo apt-get install feh
 sudo apt-get install compton
 feh --bg-center $PWD/wallpaper.png
+git clone git@github.com:krusipo/cinnamon-i3.git
+cd cinnamon-i3
+npm install
+node_modules/grunt/bin/grunt
+grunt debian
+sudo dpkg -i dist/debian/cinnamon-i3_0.5.0-1_amd64.deb
+sudo apt-get -f install
+rm -rf cinnamon-i3
 sudo apt-get install libanyevent-i3-perl
--- spacemacs/install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
