ed8d22cda196e85d11a9d7ffd8636515174c70a5
Will Simpson <wsimpson@email.arizona.edu>
2019-05-14 17:08:02 -0700

    Add Python stuff.

---
+++ python/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p ~/.virtualenvs/
+sudo apt-get install python-pip
+pip install setuptools virtualenv virtualenvwrapper
---
+++ python/python.bash
@@ -0,0 +1,3 @@
+export WORKON_HOME=~/.virtualenvs
+VIRTUALENVWRAPPER_PYTHON='/usr/bin/python'
+source $HOME/.local/bin/virtualenvwrapper.sh
