ec05ffcf5a05ff979b3f5c68c57daa8ac26e9223
Will Simpson <wsimpson@email.arizona.edu>
2019-06-05 13:25:52 -0700

    Add kubectl aliases and completion for bash.

--- bash/bash.bash
+++ bash/bash.bash
@@ -1,5 +1,13 @@
 set -o vi
 export VISUAL=vim
 
+k8s_aliases() {
+    if hash kubectl 2>/dev/null; then
+        source <(kubectl completion bash | sed s/kubectl/k/g)
+        alias k=kubectl
+    fi
+}
+
 # Aliases
 alias pass-generate="pass generate -c"
+k8s_aliases
