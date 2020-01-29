5fca33386e750d16232bc951db89b2022e14bf36
Will Simpson <wsimpson@email.arizona.edu>
2020-01-29 12:02:12 -0700

    Update install script to use Emacs 26.

--- spacemacs/install.sh
+++ spacemacs/install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo apt-get install -y emacs socat
+sudo add-apt-repository -y ppa:kelleyk/emacs
+sudo apt update
+sudo apt-get install -y emacs26 socat
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
