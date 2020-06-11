30afe3ea161156a7bde6fe05e72f614f776acc97
Will Simpson <wsimpson@email.arizona.edu>
2020-06-11 16:57:38 -0700

    Update KEYCLOAK_URL variable and export USER_GATEWAY_URL

--- bin/nafigos-env.sh
+++ bin/nafigos-env.sh
@@ -3,12 +3,12 @@ ORIGINAL_CONTEXT=`kubectl config current-context`
 if [[ "$ORIGINAL_CONTEXT" != "kind-service-cluster" ]]; then
 	kubectl config use-context kind-service-cluster
 fi
-export KEYCLOAK_URL=$(kubectl get po -l istio=ingressgateway -n istio-system -o jsonpath="{.items[0].status.hostIP}"):$(kubectl get service keycloak -o jsonpath="{.spec.ports[?(@.name==\"8080\")].nodePort}")
+export KEYCLOAK_URL=$(kubectl get po -l istio=ingressgateway -n istio-system -o jsonpath="{.items[0].status.hostIP}"):$(kubectl get service keycloak -o jsonpath="{.spec.ports[?(@.name==\"keycloak\")].nodePort}")
 export SERVICE_GATEWAY_URL=$(kubectl get po -l istio=ingressgateway -n istio-system -o jsonpath="{.items[0].status.hostIP}"):$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath="{.spec.ports[?(@.name==\"http2\")].nodePort}")
 export NAFIGOS_API=$SERVICE_GATEWAY_URL
 
 kubectl config use-context kind-user-cluster
-USER_GATEWAY_URL=$(kubectl get po -l istio=ingressgateway -n istio-system -o jsonpath="{.items[0].status.hostIP}"):$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath="{.spec.ports[?(@.name==\"http2\")].nodePort}")
+export USER_GATEWAY_URL=$(kubectl get po -l istio=ingressgateway -n istio-system -o jsonpath="{.items[0].status.hostIP}"):$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath="{.spec.ports[?(@.name==\"http2\")].nodePort}")
 
 
 echo "KEYCLOAK_URL=$KEYCLOAK_URL"
