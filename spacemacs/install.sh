94ff5b623b58522c8625c234872cca4965ca4a48
Will Simpson <wsimpson@email.arizona.edu>
2020-06-13 16:18:37 -0700

    Install Emacs from main repo rather than PPA.

--- spacemacs/install.sh
+++ spacemacs/install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sudo add-apt-repository -y ppa:kelleyk/emacs
-sudo apt update
-sudo apt-get install -y emacs26 socat
+sudo apt-get install -y emacs socat
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
