9e030e27b115fd12bf38cf3c417a44872123b824
Will Simpson <wsimpson@email.arizona.edu>
2020-06-13 22:05:55 -0700

    Add install script for Kubernetes tools.

---
+++ k8s/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.8.1/kind-$(uname)-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
