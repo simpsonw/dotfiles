fba03feb7621b178498c092f7bf70f90eed37749
Will Simpson <wsimpson@email.arizona.edu>
2020-06-13 22:09:10 -0700

    Removed unused directories.

--- gnome-terminal-colors/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd "${0%/*}"
-sudo apt-get install dconf-cli
-git clone https://github.com/dracula/gnome-terminal.git
-cd gnome-terminal && ./install.sh
--- rxvt-unicode/install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo apt-get install rxvt-unicode-256color
