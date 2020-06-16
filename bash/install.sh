a6bfe9372b514837d491aa57af4586069eb94ecc
Will Simpson <wsimpson@email.arizona.edu>
2020-06-16 10:41:53 -0700

    Install fzf with Bash tools.

--- bash/install.sh
+++ bash/install.sh
@@ -4,3 +4,4 @@ find `pwd` -name *.bash -exec echo "source" {} \; > bash/dotfiles.bashrc
 chmod +x $PWD/bash/dotfiles.bashrc
 grep -q -F "source $PWD/bash/dotfiles.bashrc" ~/.bashrc || echo "source $PWD/bash/dotfiles.bashrc" >> ~/.bashrc
 grep -q -F "export PATH=\$PATH:$PWD/bin/" ~/.bashrc || echo "export PATH=\$PATH:$PWD/bin/" >> ~/.bashrc
+sudo apt install fzf
