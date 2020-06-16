9239da57ef8a818f89dd81871d79b303d700ad91
Will Simpson <wsimpson@email.arizona.edu>
2020-06-16 10:41:25 -0700

    Switch to develop branch of Spacemacs and fix font.

--- spacemacs/install.sh
+++ spacemacs/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt-get install -y emacs socat
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+git clone --branch develop https://github.com/syl20bnr/spacemacs ~/.emacs.d
